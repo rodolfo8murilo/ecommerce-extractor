f203c5de9307e895a64b4ab7671be9e99d9d97d4
Rodolfo Murilo Barbosa Moura <rodolfo8murilo@gmail.com>
2026-03-18 16:06:31 -0300

    update-pipeline

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -1,76 +1,71 @@
+# pylint: disable=too-few-public-methods
+
 import logging
+
 from scrapy.exceptions import DropItem
+
 from crawler.database import ProductRepository
 
-logging.basicConfig(level=logging.DEBUG)
-    
+
+logger = logging.getLogger(__name__)
+
+
 class CleanDataPipeline:
-
-    def process_item(self, item, spider):
-
+    def process_item(self, item, spider):  # pylint: disable=unused-argument
         item["product_name"] = item["product_name"].strip()
-
-        return item    
-
+        return item
 
 
 class ValidatePipeline:
-
-    def process_item(self, item, spider):
-
+    def process_item(self, item, spider):  # pylint: disable=unused-argument
         if not item.get("product_id"):
-            logging.info("Missing product_id")
+            logger.info("Missing product_id")
             raise DropItem("Missing product_id")
 
         if not item.get("price"):
-            logging.info("Missing price")
+            logger.info("Missing price")
             raise DropItem("Missing price")
-        
+
         if not item.get("product_url"):
-            logging.info("Missing product_url")
+            logger.info("Missing product_url")
             raise DropItem("Missing product_url")
-        
+
         if not item.get("company_id"):
-            logging.info("Missing company_id")
+            logger.info("Missing company_id")
             raise DropItem("Missing company_id")
-        
+
         if not item.get("company_name"):
-            logging.info("Missing company_name")
+            logger.info("Missing company_name")
             raise DropItem("Missing company_name")
 
         return item
-    
-class DuplicatesPipeline:
 
-    def open_spider(self, spider):
+
+class DuplicatesPipeline:
+    def __init__(self):
+        self.repo = None
+
+    def open_spider(self, spider):  # pylint: disable=unused-argument
         self.repo = ProductRepository()
 
-    def process_item(self, item, spider):
-        if self.repo.get_product_by_id(item["product_id"]) != None:
-            logging.info("Missing company_name")
+    def process_item(self, item, spider):  # pylint: disable=unused-argument
+        if self.repo.get_product_by_id(item["product_id"]) is not None:
+            logger.info("Duplicate item")
             raise DropItem("Duplicate item")
+
         return item
+
 
 class SaveToDatabasePipeline:
+    def __init__(self):
+        self.repo = None
 
-    def open_spider(self, spider):
+    def open_spider(self, spider):  # pylint: disable=unused-argument
         self.repo = ProductRepository()
 
-    def process_item(self, item, spider):
-
-        self.repo.insert_product(
-            product_id=item["product_id"],
-            product_name=item["product_name"],
-            price=item["price"],
-            price_with_discount=item["price_with_discount"],
-            old_price=item["old_price"],
-            product_url=item["product_url"],
-            company_id=item['company_id'],
-            company_name=item['company_name']
-        )
-        
+    def process_item(self, item, spider):  # pylint: disable=unused-argument
+        self.repo.insert_product(item)
         return item
 
-    def close_spider(self, spider):
+    def close_spider(self, spider):  # pylint: disable=unused-argument
         self.repo.close()
-    
